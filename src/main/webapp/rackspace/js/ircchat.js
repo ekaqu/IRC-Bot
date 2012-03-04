c42581201aef42746b7677cb805479b2c7a0b9ed
David Capwell <ekaqu1028@gmail.com>
2012-03-04 15:43:49 -0800

    cleaned up ircchat code.  Made it work as a chat

--- src/main/webapp/rackspace/js/ircchat.js
+++ src/main/webapp/rackspace/js/ircchat.js
@@ -1,45 +1,27 @@
-$(document).ready(function(){
-      $('#textarea').load('content.txt');
+$(document).ready( function() {
+  $('#textarea').load('content.txt');
+  $('#chatform').submit( function(event) {
+    /* stop form from submitting normally */
+    event.preventDefault();
 
-      $('#chatform').submit(function(event) {
-         /* stop form from submitting normally */
-        event.preventDefault();
+    /* get some values from elements on the page: */
+    var $form = $( this ),
+      term1 = $form.find( 'input[name="name"]' ).val(),
+      term2 = $form.find( 'input[name="content"]' ).val(),
+      url = $form.attr( 'action' );
 
-        /* get some values from elements on the page: */
-        var $form = $( this ),
-        term1 = $form.find( 'input[name="name"]' ).val(),
-        term2 = $form.find( 'input[name="content"]' ).val(),
-        url = $form.attr( 'action' );
+    $("#chat-msg").append(term1 + ": " + term2 + "\n");
 
-        /*var s = term2.split(" ");
-        var s2 = "";
-
-        for(i = 1; i < s.length; i++){
-          s2 += " " +  s[i];
-        }*/
-
-        var data = {
-          name: term1,
-          say: term2
-        };
-
-        var request = JSON.stringify(data);
-        /*var name = JSON.stringify(s[0]);
-        var msg = JSON.stringify(s2);
-         */
-        $("#chat-msg").append(data);
-
-
-        /* Send the data using post and put the results in a div */
-        $.post( url, data,
-          function(data) {
-					            alert(data);
-            var obj = jQuery.parseJSON(data);
-            var msg = obj.name + ": " + obj.say + "\n";
-            $("#chat-msg").append(msg);
-				});
-
-      });
-
-
+    /* Send the data using post and put the results in a div */
+    $.post( url, {name: term1, say: term2}, function(data) {
+      var obj = jQuery.parseJSON(data);
+      for (var i in obj) {
+        var chat = obj[i],
+          nick = chat.user.nick,
+          message = chat.message;
+        var msg = nick + ": " + message + "\n";
+        $("#chat-msg").append(msg);
+      }
     });
+  });
+});
