83343ed1213bb3e4903bd69759dd623177b35d1d
David Capwell <ekaqu1028@gmail.com>
2012-03-03 16:37:16 -0800

    fixed integration

--- src/main/java/com/wamad/servlet/Post.java
+++ src/main/java/com/wamad/servlet/Post.java
@@ -46,19 +46,7 @@ public class Post extends HttpServlet {
     response.getWriter().println("<h1>Hello Servlet</h1>");
     response.getWriter().println("session=" + request.getSession(true).getId());
     //response.getWriter().println("<h1>Hello Servlet</h1>");
-    response.sendRedirect("index.html?hello");
-  }
-
-  @Override
-  protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-    System.out.println(request.getParameter("content"));
-    response.setContentType("text/html");
-    response.setStatus(HttpServletResponse.SC_OK);
-    response.getWriter().println("<h1>Hello Servlet</h1>");
-    response.getWriter().println("session=" + request.getSession(true).getId());
-
-
-    //response.getWriter().println("<h1>Hello Servlet</h1>");
 //    response.sendRedirect("index.html?hello");
   }
+
 }
